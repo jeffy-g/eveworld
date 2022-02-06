e3c1ad96572b8d74510517f734098d5fd4dc96c4
jeffy-g <hirotom1107@gmail.com>
2022-02-06 21:51:49 +0900

    latest region map data

--- docs/config.js
+++ docs/config.js
@@ -21,7 +21,7 @@ export const WorldConfig={helperMode:!1,showAxes:!1,showGrid:!1,"Ray threshold(L
 "system size(fixed)":7,"hunter radius(LY)":.25,"hunter opacity":.4,"hunter use wireframe":!1,
 "hunter sphere division":16,"closeUp to":10,fov:50,resetCamera:()=>{},updateRegionColors:()=>{
 EVEWorld.updateRegionsColor()},worldConfig:{stepOfRotateY:.01,rotateRadiusAdjust:0,moveCamera:!1,visible:!0,dcad:2},
-"Apply texture":!0,transparent:!1,opacity:1,colorStop0:.24,colorStop1:.27,colorStop2:.41,
-blending:THREE.AdditiveBlending,depthFunc:THREE.AlwaysDepth,regionVisibility:{"Region name":!0,"Text scale":.8,
-universe:{},wormhole:{ALL:!0}},systemHunt:"tick",systemCloseUp:"fadein",closeUpEaseFanction:"easeOutSine",
-effectVolume:.5,reduceFps:!1,PixelRatio:window.devicePixelRatio};
+"Apply texture":!0,transparent:!1,opacity:1,colorStop0:.21,colorStop1:.3,colorStop2:.54,blending:THREE.AdditiveBlending,
+depthFunc:THREE.AlwaysDepth,regionVisibility:{"Region name":!0,"Text scale":.8,universe:{},wormhole:{ALL:!0}},
+systemHunt:"tick",systemCloseUp:"fadein",closeUpEaseFanction:"easeOutSine",effectVolume:.5,reduceFps:!1,
+PixelRatio:window.devicePixelRatio};
