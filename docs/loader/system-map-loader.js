b9860b64c7016fbd6bf432695c85f8bedcbc1665
jeffy-g <hirotom1107@gmail.com>
2022-05-26 23:44:58 +0900

    🎫 Introduced "es-module-shims"
    for browsers that do not yet support `importmap`

--- docs/loader/system-map-loader.js
+++ docs/loader/system-map-loader.js
@@ -5,6 +5,12 @@
   https://opensource.org/licenses/mit-license.php
  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 */
+if (!/Chrome/.test(navigator.userAgent)) {
+    const d = document;
+    const s = d.head.insertBefore(d.createElement("script"), d.head.firstElementChild);
+    s.async = true;
+    s.src = "https://ga.jspm.io/npm:es-module-shims@1.5.5/dist/es-module-shims.js";
+}
 /** @type {import("compressed-script-loader").TCSLCallbak} */
 var sysMapLoadDone = (err) => {
     sysMapLoadDone = void 0;
